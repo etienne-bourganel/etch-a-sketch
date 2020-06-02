ee7d45d3b0b913469aafde42b2d8a90941ef40a4
Etienne Bourganel <etienne.bourganel@gmail.com>
2020-06-02 16:36:24 +0200

    Add flex to container and create functions

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
+
 const container = document.querySelector("#container");
 let squareNumber = 16;
 
+container.style.display = "flex";
+container.style.flexWrap = "wrap";
 
 function createSquare (x){
     const square = document.createElement("div");
     //square.setAtrribute("id","square");
     square.style.backgroundColor = "cyan";
-    square.style.width = "25px";
-    square.style.height = "25px";
+    square.style.width = "50px";
+    square.style.height = "50px";
     square.style.borderColor = "black";
     square.style.borderWidth = "10px";
-    square.style.margin = "5px";
+    square.style.margin = "10px";
     square.textContent = x;
-    container.appendChild(square);
-    
+    square.style.display = "flex";
+    container.appendChild(square); 
+};
+
+function createLine(){
+    for (let i=1; i<=squareNumber; i++){
+        createSquare(i);
+    };
 };
 
 function createGrid(){
-    for (let i=1; i<=16; i++){
-        createSquare(i);
+    for (let i=1; i<=squareNumber; i++){
+        createLine();
     };
 };
