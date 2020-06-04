5bce1a75d92a3efd9491d85737cada709a88b433
Etienne Bourganel <etienne.bourganel@gmail.com>
2020-06-04 08:23:10 +0200

    Add variables to compute correct size for squares

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
 
 const container = document.querySelector("#container");
 let squareNumber = 16;
+let containerSize = 1000;
+let wrapBreak = 1/squareNumber*100;
+let sizeRatio = containerSize/squareNumber;
 
 container.style.display = "flex";
 container.style.flexWrap = "wrap";
+container.style.width = `${containerSize}px`;
+container.style.height = `${containerSize}px`;
+
+container.style.borderStyle = "solid";
 
 function createSquare (x){
     const square = document.createElement("div");
-    //square.setAtrribute("id","square");
     square.style.backgroundColor = "cyan";
-    square.style.width = "50px";
-    square.style.height = "50px";
+    square.style.width = `${sizeRatio-1}px`;
+    square.style.height = `${sizeRatio-1}px`;
+    square.style.borderStyle = "solid";
     square.style.borderColor = "black";
-    square.style.borderWidth = "10px";
-    square.style.margin = "10px";
+    square.style.borderWidth = "0px";
     square.textContent = x;
+    square.style.flexBasis = `${wrapBreak}%`;
     square.style.display = "flex";
     container.appendChild(square); 
 };
