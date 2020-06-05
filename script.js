91897268c644acbfe9fea28f15780d178003d343
Etienne Bourganel <etienne.bourganel@gmail.com>
2020-06-05 13:50:34 +0200

    Add functions to create and clear grid using css variable

--- script.js
+++ script.js
@@ -1,32 +1,25 @@
-
 const container = document.querySelector("#container");
-let squareNumber = 16;
+container.classList.add("container");
+const gridSize = 64;
 
-container.style.display = "flex";
-container.style.flexWrap = "wrap";
+//let root = document.documentElement;
+document.documentElement.style.setProperty("--gridSize", gridSize); 
 
-function createSquare (x){
-    const square = document.createElement("div");
-    //square.setAtrribute("id","square");
-    square.style.backgroundColor = "cyan";
-    square.style.width = "50px";
-    square.style.height = "50px";
-    square.style.borderColor = "black";
-    square.style.borderWidth = "10px";
-    square.style.margin = "10px";
-    square.textContent = x;
-    square.style.display = "flex";
-    container.appendChild(square); 
+
+function createCell() {
+    const cell = document.createElement("div");
+    container.appendChild(cell);
+    cell.classList.add("cell");
 };
 
-function createLine(){
-    for (let i=1; i<=squareNumber; i++){
-        createSquare(i);
+function createGrid () {
+    for (let i = 1; i<= gridSize*gridSize; i++){
+        createCell();
     };
 };
 
-function createGrid(){
-    for (let i=1; i<=squareNumber; i++){
-        createLine();
-    };
+function clearGrid () {
+   container.innerHTML = "";
 };
+
+
